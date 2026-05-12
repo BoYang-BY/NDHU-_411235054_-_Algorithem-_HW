65c674b7ab160177f041d6da1d0edbc12240c5d8
BO-YANG,XU <411235054@gms.ndhu.edu.tw>
2026-05-12 20:55:13 +0800

    Add files via upload

---
+++ week10_376_程式碼.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int> up(n, 1);
+        vector<int> down(n, 1);
+
+        for(int i = 1; i < n; i++) {
+
+            for(int j = 0; j < i; j++) {
+
+                if(nums[i] > nums[j]) {
+                    up[i] = max(up[i], down[j] + 1);
+                }
+
+                else if(nums[i] < nums[j]) {
+                    down[i] = max(down[i], up[j] + 1);
+                }
+            }
+        }
+
+        return max(up[n-1], down[n-1]);
+    }
+};
